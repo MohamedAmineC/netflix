61304d84b1055e9a343f5792d640919b8c893512
Mohmaed Amine Chalouati <mohamedaminechalouati@gmail.com>
2023-04-16 13:24:31 +0200

    auth screen UI

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>My netflix clone page</h1>
+      <h1 className='text-3xl text-white'>My netflix clone page</h1>
     </main>
   )
 }
